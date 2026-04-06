2162fbb96acde2fdc1dd48c1bfced8568c1bb05e
valdesdario9-ai <valdesdario9@gmail.com>
2026-04-06 09:22:57 -0300

    Update ISupabaseService.cs

--- Services/ISupabaseService.cs
+++ Services/ISupabaseService.cs
@@ -1,1 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarniceriaWhatsApp.Models;
+using System.IO;
 
+namespace CarniceriaWhatsApp.Services
+{
+    public interface ISupabaseService
+    {
+        Task<List<Producto>> ObtenerProductosAsync();
+        Task<Producto> ObtenerProductoAsync(int id);
+        Task<Producto> CrearProductoAsync(Producto producto);
+        Task<Producto> ActualizarProductoAsync(int id, Producto producto);
+        Task<bool> EliminarProductoAsync(int id);
+        Task<string> SubirImagenAsync(Stream imagenStream, string fileName, string contentType);
+        
+        Task<ConfiguracionCarniceria> ObtenerConfiguracionAsync();
+        Task<ConfiguracionCarniceria> ActualizarConfiguracionAsync(ConfiguracionCarniceria config);
+    }
+}
