366b71cb1f872c98bacff72923ebea3e8e8510f4
valdesdario9-ai <valdesdario9@gmail.com>
2026-04-06 08:55:04 -0300

    Create Privacy.cshtml.cs

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarniceriaWhatsApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
