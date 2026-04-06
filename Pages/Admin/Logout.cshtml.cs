82fda30e1c93294f2c32fcc8448d1dad62650d0a
valdesdario9-ai <valdesdario9@gmail.com>
2026-04-06 11:13:36 -0300

    Create Logout.cshtml.cs

---
+++ Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarniceriaWhatsApp.Pages.Admin
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            HttpContext.Session.Remove("AdminLogged");
+            return RedirectToPage("/Admin/Login");
+        }
+    }
+}
