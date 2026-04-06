b6342aa14635321a3983a314b8484ae76e5bcb20
valdesdario9-ai <valdesdario9@gmail.com>
2026-04-06 11:14:06 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@ using CarniceriaWhatsApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar servicios
 builder.Services.AddHttpClient<ISupabaseService, SupabaseService>();
 builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
-// Configurar pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -18,6 +22,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseSession();
 app.MapRazorPages();
 
 app.Run();
